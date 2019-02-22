5356259d27a454b16cd51dcfae17eb3a94414b99
d.mui2383 <d.mui2383@gmail.com>
2019-02-22 12:57:44 -0500

    burger created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
+import classes from './Layout.css'
 
 const layout = (props) => (
   <Aux>
     <div>ToolBar, Sidedrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
