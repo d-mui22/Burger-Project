5c7ab5c9d55596a408d49efb9a8f42e8c2553890
d.mui2383 <d.mui2383@gmail.com>
2019-03-17 22:18:04 -0400

    changed OrderSummary and Modal to class and added shouldComponentUpdate to Modal.js

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,40 @@
-import React, {Fragment} from 'react'
+import React, {Component, Fragment} from 'react'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(ingredientKey => {
-      return (
-        <li key={ingredientKey}>
-          <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: <strong>{props.ingredients[ingredientKey]}</strong>
-        </li>)
-    })
-  return(
-    <Fragment>
-      <div style={{textAlign: 'center'}}>
-        <h3 style={{marginBottom: '0'}}>Your Order</h3>
-        <h4 style={{textDecorationLine: 'underline', marginTop: '0'}}>Selected Ingredients</h4>
-      </div>
-      <div style={{marginLeft: '30%'}}>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p>Price: <mark>$<strong>{props.totalPrice.toFixed(2)}</strong></mark></p>
-      </div>
-      <div style={{textAlign: 'center'}}>
-        <Button
-          clicked={props.continueOrder}
-          btnType='Success'>Continue</Button>
-        <Button
-          clicked={props.cancelOrder}
-          btnType='Fail'>Cancel</Button>
-      </div>
-    </Fragment>
-  )
+class OrderSummary extends Component {
+  
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(ingredientKey => {
+        return (
+          <li key={ingredientKey}>
+            <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: <strong>{this.props.ingredients[ingredientKey]}</strong>
+          </li>)
+      })
+
+    return (
+      <Fragment>
+        <div style={{textAlign: 'center'}}>
+          <h3 style={{marginBottom: '0'}}>Your Order</h3>
+          <h4 style={{textDecorationLine: 'underline', marginTop: '0'}}>Selected Ingredients</h4>
+        </div>
+        <div style={{marginLeft: '30%'}}>
+          <ul>
+            {ingredientSummary}
+          </ul>
+          <p>Price: <mark>$<strong>{this.props.totalPrice.toFixed(2)}</strong></mark></p>
+        </div>
+        <div style={{textAlign: 'center'}}>
+          <Button
+            clicked={this.props.continueOrder}
+            btnType='Success'>Continue</Button>
+          <Button
+            clicked={this.props.cancelOrder}
+            btnType='Fail'>Cancel</Button>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,34 @@
-import React, {Fragment} from 'react'
+import React, {Component, Fragment} from 'react'
 import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-  return(
-    <Fragment>
-      <Backdrop
-        show={props.show}
-        clicked={props.cancelOrder}
-      />
-      <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-        {props.children}
-      </div>
-    </Fragment>
-  )
+class Modal extends Component {
+  // OrderSummary should ONLY update if MODAL is visible. Without shouldComponentUpdate
+  // OrderSummary would update everytime ingredient or price is changed even though
+  // OrderSummary is not visible. With shouldComponentUpdate it ONLY updates OrderSummary if
+  // this.props.show for Modal is TRUE! Which is when we want an update to be rendered.
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.shows
+  }
+  
+  render () {
+    return(
+      <Fragment>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.cancelOrder}
+        />
+        <div
+        className={classes.Modal}
+        style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1' : '0'
+        }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default modal
+export default Modal
